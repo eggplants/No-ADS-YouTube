ea4bc6e7f0d45efdf130b258a2d23dae15440688
eggplants <w10776e8w@yahoo.co.jp>
2023-11-10 01:23:05 +0900

    more readable

--- NoAdsYt.user.js
+++ NoAdsYt.user.js
@@ -10,19 +10,65 @@
 // @grant        none
 // ==/UserScript==
 
-let ogVolume=1,pbRate = 1;setInterval(()=>{
-    if(document.getElementsByClassName("video-stream html5-main-video")[0]!==undefined){let ad = document.getElementsByClassName("video-ads ytp-ad-module")[0];
-        let vid = document.getElementsByClassName("video-stream html5-main-video")[0];if(ad==undefined){pbRate = vid.playbackRate}
-        let closeAble = document.getElementsByClassName("ytp-ad-overlay-close-button");for(let i=0;i<closeAble.length;i++){closeAble[i].click()}
-        if(document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0]!==undefined)
-        {let sideAd=document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0];sideAd.style.display="none"}
-        if(document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0]!==undefined)
-        {let sideAd_ = document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0];sideAd_.style.display="none"}
-        if(document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0]!==undefined){let skipBtn=document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0];skipBtn.click()}
-        if(document.getElementsByClassName("ytp-ad-message-container")[0]!==undefined){let incomingAd=document.getElementsByClassName("ytp-ad-message-container")[0];incomingAd.style.display="none"}
-        if(document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0]!==undefined){document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0].remove()}
-        if(ad!==undefined){if(ad.children.length>0){if(document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0]!==undefined)
-        {vid.playbackRate=16;vid.muted=true;}}}if(document.getElementById("masthead-ad")!==null){let headerAd=document.getElementById("masthead-ad");headerAd.remove()}
-        if(document.getElementsByTagName("ytd-ad-slot-renderer")[0]!==undefined){let rightSideAd=document.getElementsByTagName("ytd-ad-slot-renderer")[0];rightSideAd.remove()}
-        if(document.getElementsByTagName("ytd-reel-shelf-renderer")[0]!==undefined){let rightSideShorts=document.getElementsByTagName("ytd-reel-shelf-renderer")[0];rightSideShorts.remove()}}
-},100);
+let ogVolume = 1;
+let pbRate = 1;
+
+const get = (className) => document.getElementsByClassName(className);
+const getFirst = (className) => get(className)[0];
+const isExistsElm = (elm) => ![undefined, null].includes(elm);
+const isExists = (className) => isExistsElm(getFirst(className));
+
+setInterval(() => {
+  if (!isExists("video-stream html5-main-video")) {
+    return;
+  }
+
+  const ad = getFirst("video-ads ytp-ad-module");
+  const vid = getFirst("video-stream html5-main-video");
+  // if (!isExistsElm(ad)) { pbRate = vid.playbackRate; }
+  if (
+    isExistsElm(ad) &&
+    ad.children.length > 0 &&
+    isExists("ytp-ad-text ytp-ad-preview-text")
+  ) {
+    vid.playbackRate = 16;
+    vid.muted = true;
+  }
+
+  // closeAble
+  Array.from(get("ytp-ad-overlay-close-button")).forEach((e) => e.click());
+
+  // skipBtn
+  getFirst("ytp-ad-text ytp-ad-skip-button-text")?.click();
+
+  const sideAd1 = getFirst(
+    "style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement",
+  );
+  if (!isExistsElm(sideAd1)) {
+    sideAd1.style.display = "none";
+  }
+
+  const sideAd2 = getFirst(
+    "style-scope ytd-item-section-renderer sparkles-light-cta",
+  );
+  if (!isExistsElm(sideAd2)) {
+    sideAd2.style.display = "none";
+  }
+
+  const incomingAd = getFirst("ytp-ad-message-container");
+  if (!isExistsElm(incomingAd)) {
+    incomingAd.style.display = "none";
+  }
+
+  // bottomSideAd
+  getFirst("style-scope ytd-companion-slot-renderer")?.remove();
+
+  // headerAd
+  document.getElementById("masthead-ad")?.remove();
+
+  // rightSideAd
+  document.getElementsByTagName("ytd-ad-slot-renderer")[0]?.remove();
+
+  // rightSideShorts
+  document.getElementsByTagName("ytd-reel-shelf-renderer")[0]?.remove();
+}, 100);
